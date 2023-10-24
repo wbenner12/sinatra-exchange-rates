8502a5477f2d9791e7e5f88c334c69d0458d8744
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-24 21:15:46 +0000

    donezo

--- app.rb
+++ app.rb
@@ -27,17 +27,41 @@ get("/") do
   erb(:homepage)
 end
 
-get '/convert/:from/:to' do
-  from_currency = params['from']
-  to_currency = params['to']
+get("/:from_currency") do
+  @from_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["CURRENCY_KEY"]}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @symbols = parsed_data.fetch("currencies").keys
+
+  # render a view template where I show the symbols
+  erb(:middlepage)
+end
+
+
+get("/:from_currency/:to_currency") do
+  @from_currency = params.fetch("from_currency")
+  @to_currency = params.fetch("to_currency")
 
   # Use exchangerate.host API to get exchange rates
-  exchange_rates_url = "https://api.apilayer.com/exchangerates/#{from_currency}/#{to_currency}"
-  data = JSON.parse(open(exchange_rates_url).read)
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["CURRENCY_KEY"]}&from=#{@from_currency}&to=#{@to_currency}&amount=1"
 
-  # Extract the exchange rate
-  exchange_rate = data['rates'][to_currency]
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @exchange_rate = parsed_data.fetch("result")
+
 
   # Render a view template to show the conversion
-  erb :conversion, locals: { from_currency: from_currency, to_currency: to_currency, exchange_rate: exchange_rate }
+  erb(:conversion)
 end
